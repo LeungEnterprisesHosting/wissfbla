2896778840f28174d2a6a4ac2f87b2a1815003db
nathanhleung <nathanhleung@users.noreply.github.com>
2015-10-14 02:05:37 +0000

    Remove signup

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,14 @@ $(document).ready(function() {
 
   $(window).scroll(function() {
     if ($(window).scrollTop() > 90) {
-      if (!$('.hamburger-navbar').hasClass('scrolled')) {
+      if (!$('.hamburger-navbar').hasClass('scrolled') && !$('.hamburger-container').hasClass('scrolled')) {
         $('.hamburger-navbar').addClass('scrolled');
+        $('.hamburger-container').addClass('scrolled');
       }
     } else {
-      if ($('.hamburger-navbar').hasClass('scrolled')) {
+      if ($('.hamburger-navbar').hasClass('scrolled') && $('.hamburger-container').hasClass('scrolled')) {
         $('.hamburger-navbar').removeClass('scrolled');
+        $('.hamburger-container').removeClass('scrolled');
       }
     }
   });
